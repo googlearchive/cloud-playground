e057bbe09d30a6dcafdd3a14fd33acc63df63778
Fred Sauer <fredsa@google.com>
2012-09-25 12:29:32 -0700

    use lib_config to make mimic's create_tree_func configurable via appengine_config.py

--- mimic.py
+++ mimic.py
@@ -32,7 +32,14 @@ from __mimic import datastore_tree
 from __mimic import mimic
 from __mimic import target_env
 
+from google.appengine.api import lib_config
+
 _SEPARATOR = '-' * 50 + '\n'
+
+
+_config = lib_config.register('mimic', {
+    'CREATE_TREE_FUNC': datastore_tree.DatastoreTree,
+    })
 
 
 class Mimic(object):
@@ -54,7 +61,7 @@ class Mimic(object):
           and os.environ['PATH_INFO'] == '/'):
         yield self._RedirectResponse('/bliss')
         return
-      mimic.RunMimic(create_tree_func=datastore_tree.DatastoreTree)
+      mimic.RunMimic(create_tree_func=_config.CREATE_TREE_FUNC)
     except target_env.TargetAppError, err:
       yield self._ExceptionResponse(err.FormattedException())
       return
