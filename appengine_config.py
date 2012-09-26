0bf4d0bf645eaea4650a6f39566d39274564d713
Fred Sauer <fredsa@google.com>
2012-09-26 12:12:16 -0700

    dynamically selected DatastoreTree or UrlFetchTree based on current app id

--- appengine_config.py
+++ appengine_config.py
@@ -1,20 +1,21 @@
 import re
-#import urlfetch_tree
+import urlfetch_tree
 
 from __mimic import mimic
 from __mimic import datastore_tree
 
+from google.appengine.api import app_identity
 
-mimic_CREATE_TREE_FUNC = datastore_tree.DatastoreTree
-#mimic_CREATE_TREE_FUNC = urlfetch_tree.UrlFetchTree
+urlfetch_tree_SOURCE_CODE_APP_ID = 'try-appengine'
+
+if app_identity.get_application_id() == urlfetch_tree_SOURCE_CODE_APP_ID:
+  mimic_CREATE_TREE_FUNC = datastore_tree.DatastoreTree
+else:
+  mimic_CREATE_TREE_FUNC = urlfetch_tree.UrlFetchTree
 
 mimic_PROJECT_NAME_COOKIE = '_bliss_project'
 
 mimic_PROJECT_NAME_FROM_PATH_INFO_RE = re.compile('/bliss/p/(.+?)/')
-
-
-#urlfetch_tree_SOURCE_CODE_APP_ID = 'try-appengine'
-
 
 def namespace_manager_default_namespace_for_request():
   return mimic.GetNamespace()
