e48330e1e20967f200fd994b9be8d06be6e28c2b
Fred Sauer <fredsa@google.com>
2012-11-21 16:38:21 -0800

    typo

--- secret.py
+++ secret.py
@@ -11,7 +11,7 @@ class PlaygroundSecret(ndb.Model):
   """A model which stores secret keys."""
   secret_key = ndb.StringProperty(indexed=False)
   created = ndb.DateTimeProperty(auto_now_add=True, indexed=False)
-  udpated = ndb.DateTimeProperty(auto_now=True, indexed=False)
+  updated = ndb.DateTimeProperty(auto_now=True, indexed=False)
 
 
 def GetSecret(key_name, entropy):
