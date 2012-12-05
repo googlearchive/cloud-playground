34ef73270da8bdf489a9347e6ab5afc04ef4c820
Fred Sauer <fredsa@google.com>
2012-12-05 15:14:04 -0800

    test login/logout buttons

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -9,6 +9,7 @@ describe('cloud playground app', function() {
   });
 
 
+
   afterEach(function() {
     //sleep(1.5);
     //pause();
@@ -22,6 +23,65 @@ describe('cloud playground app', function() {
 
   it('should automatically redirect to /playground/', function() {
     expect(browser().location().url()).toBe("/playground/");
+  });
+
+
+  describe('login behavior', function() {
+
+    var current_url = 'http://localhost:7070/playground/';
+
+    function logout() {
+      expect(browser().window().href()).toBe(current_url);
+      browser().navigateTo('/_ah/login?action=Logout&continue=' + current_url);
+      expect(browser().window().href()).toBe(current_url);
+    }
+
+    function login(admin) {
+      expect(browser().window().href()).toBe(current_url);
+      browser().navigateTo('/_ah/login?email=test@example.com&admin=' +
+                           (admin ? 'True' : 'False') +
+                           '&action=Login&continue=' + current_url);
+      expect(browser().window().href()).toBe(current_url);
+    }
+
+
+    it('should only provide login button when the user is not logged in', function() {
+      logout();
+      expect(element('button:contains("login")').count()).toEqual(1);
+      expect(element('button:contains("login")').css('display')).toEqual('inline-block');
+      expect(element('button:contains("login")').height()).toBeGreaterThan(0);
+      login(true);
+      expect(element('button:contains("login")').count()).toEqual(1);
+      expect(element('button:contains("login")').css('display')).toEqual('none');
+      // TODO figure out why this fails:
+      //expect(element('button:contains("login")').height()).toEqual(0);
+    });
+
+
+    it('should only provide logout button when the user is logged in', function() {
+      login(true);
+      expect(element('button:contains("logout")').count()).toEqual(1);
+      expect(element('button:contains("logout")').css('display')).toEqual('inline-block');
+      expect(element('button:contains("logout")').height()).toBeGreaterThan(0);
+      logout();
+      expect(element('button:contains("logout")').count()).toEqual(1);
+      expect(element('button:contains("logout")').css('display')).toEqual('none');
+      // TODO figure out why this fails:
+      //expect(element('button:contains("logout")').height()).toEqual(0);
+    });
+
+
+    it('should only provide admin links when user is logged in as an admin', function() {
+      login(false);
+      expect(element('[ng-click="big_red_button()"]').height()).toEqual(0);
+      expect(element('[ng-click="datastore_admin()"]').height()).toEqual(0);
+      expect(element('[ng-click="memcache_admin()"]').height()).toEqual(0);
+      login(true);
+      expect(element('[ng-click="big_red_button()"]').height()).toBeGreaterThan(0);
+      expect(element('[ng-click="datastore_admin()"]').height()).toBeGreaterThan(0);
+      expect(element('[ng-click="memcache_admin()"]').height()).toBeGreaterThan(0);
+    });
+
   });
 
 
