969eee0fb073d047e053c7de0f347e76e17c4ceb
Fred Sauer <fredsa@google.com>
2013-01-28 12:39:40 -0800

    Revert "update testacular configuration files to accomodate angular version change"
    This reverts commit 179c62a1cc75d18282129f734ada97caafde0181.

--- config/testacular.conf.js
+++ config/testacular.conf.js
@@ -6,9 +6,9 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/lib/angular*/angular.js',
-  'app/lib/angular*/angular-*.js',
-  'app/lib/angular*/angular-mocks.js',
+  'app/lib/angular/angular.js',
+  'app/lib/angular/angular-*.js',
+  'test/lib/angular/angular-mocks.js',
   'app/js/**/*.js',
   'test/unit/**/*.js'
 ];
