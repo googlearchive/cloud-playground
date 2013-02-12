be6fd06e158fca2dfa63974f9b83b85172672ac6
Takashi Matsuo <tmatsuo@google.com>
2013-02-12 00:23:21 -0800

    Added a test for prompt_new_file.

--- test/unit/mocks.js
+++ test/unit/mocks.js
@@ -3,6 +3,39 @@ angular.module('mocks.dialog', [])
 
 .provider('$dialog', function() {
   return {
-    $get: function() {},
+    $get: function() {
+      var dialogMock = {};
+      var expectation;
+      var userInput;
+
+      dialogMock.shouldBeCalledWith = function(arg) {
+        expectation = arg;
+      };
+
+      dialogMock.willCloseWith = function(mockInput) {
+        userInput = mockInput;
+      };
+
+      dialogMock.dialog = function(arg) {
+        if (expectation &&
+            (expectation.controller != arg.controller ||
+                expectation.templateUrl != arg.templateUrl)) {
+          throw Error('Expected:' + prettyPrint(expectation) + 'Actual:' +
+                          prettyPrint(arg));
+        }
+        return {'open': function() {
+          return {'then': function(callBack) {
+            callBack(userInput);
+          }};
+        }};
+      };
+      return dialogMock;
+    }
   };
-})
+});
+
+function prettyPrint(data) {
+  return (angular.isString(data) || angular.isFunction(data) || data instanceof RegExp)
+    ? data
+    : angular.toJson(data);
+}
