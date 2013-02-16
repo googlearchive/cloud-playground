aec7054c9b0700020e9d2fe775bba4741e20fe60
Fred Sauer <fredsa@google.com>
2013-02-15 16:05:51 -0800

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,15 @@ set -ue
 
 $(dirname $0)/sdkapi.sh
 
+APPCFG=$(which appcfg.py) \
+  || (echo "ERROR: appcfg.py must be in your PATH"; exit 1)
+while [ -L $APPCFG ]
+do
+  APPCFG=$(readlink $APPCFG)
+done
+
+SDK_HOME=$(dirname $APPCFG)
+
 function deploy() {
   echo -e "\n*** Rolling back any pending updates (just in case) ***\n"
   appcfg.py --oauth2 $* rollback .
@@ -17,7 +26,7 @@ if [ $( echo "$*" | egrep -- '-A'\|'--application=' >/dev/null; echo $? ) == 0 ]
 then
   deploy $*
 else
-  appids=$(SERVER_SOFTWARE=$0 APPLICATION_ID=foo python -c 'import settings; settings.PrintAppIdsInMap()')
+  appids=$(PYTHONPATH=$SDK_HOME SERVER_SOFTWARE=$0 APPLICATION_ID=foo python -c 'import settings; settings.PrintAppIdsInMap()')
   for appid in $appids
   do
     deploy -A $appid $*
