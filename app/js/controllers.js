12d3afc782d5cf5fcbd6bd56e492db6fc3d6f6f4
Fred Sauer <fredsa@google.com>
2013-04-04 14:53:18 -0700

    an attempt to clear undo history when switching files

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -525,6 +525,8 @@ function ProjectController($scope, $browser, $http, $routeParams, $window,
                 $scope.run();
               }
           });
+          // clear undo history
+          $scope.codeMirror.getDoc().clearHistory();
         }
       });
     });
