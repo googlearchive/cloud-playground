e7780c6d011baf50d308d076c96cf8feb9b49759
Fred Sauer <fredsa@google.com>
2013-04-23 17:52:12 -0700

    check that karma is installed

--- scripts/go.sh
+++ scripts/go.sh
@@ -31,6 +31,14 @@ function quit() {
   exit 0
 }
 
+which karma >/dev/null
+if [ "$?" != "0" ]
+then
+  echo "Please install Karma using 'npm install -g karma'"
+  echo "See http://karma-runner.github.io/"
+  exit 1
+fi
+
 trap quit SIGINT
 
 XTERM_ARGS="-fa 'Mono' -fs 10"
