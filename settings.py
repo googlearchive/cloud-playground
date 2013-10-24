5829afb4345d09a04fca61e51624f580a95c408d
Fred Sauer <fredsa@google.com>
2013-10-23 21:26:27 -0700

    remove None.appspot.com from settings.PLAYGROUND_HOSTS

--- settings.py
+++ settings.py
@@ -80,16 +80,18 @@ DEFAULT_EXPIRATION_SECONDS = 604800
 SKIP_EXTENSIONS = ('swp', 'pyc', 'svn')
 
 if _DEV_MODE:
-  PLAYGROUND_HOSTS = ('localhost:8080', '127.0.0.1:8080',
+  PLAYGROUND_HOSTS = ['localhost:8080', '127.0.0.1:8080',
                       # port 7070 for karma e2e test
                       'localhost:7070', '127.0.0.1:7070',
-                      app_identity.get_default_version_hostname())
+                      app_identity.get_default_version_hostname()]
   # PLAYGROUND_USER_CONTENT_HOST = backends.get_hostname('user-content-backend')
   PLAYGROUND_USER_CONTENT_HOST = None
   MIMIC_HOST = backends.get_hostname('exec-code-backend')
 else:
-  PLAYGROUND_HOSTS = ('{}.appspot.com'.format(appids.PLAYGROUND_APP_ID),
-                      '{}.appspot.com'.format(appids.PLAYGROUND_APP_ID_ALIAS))
+  PLAYGROUND_HOSTS = ['{}.appspot.com'.format(appids.PLAYGROUND_APP_ID)]
+  if appids.PLAYGROUND_APP_ID_ALIAS:
+    PLAYGROUND_HOSTS.append('{}.appspot.com'
+                            .format(appids.PLAYGROUND_APP_ID_ALIAS))
   # PLAYGROUND_USER_CONTENT_HOST = ('{0}-dot-{1}.appspot.com'
   #                                 .format(USER_CONTENT_PREFIX,
   #                                         appids.PLAYGROUND_APP_ID))
