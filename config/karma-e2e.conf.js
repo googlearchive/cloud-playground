dc559187148d8026486d53fc63e1623b9474dce1
Takashi Matsuo <tmatsuo@google.com>
2013-11-21 10:04:09 -0800

    Update karma config files.
    Reviewed in https://codereview.appspot.com/28690043/

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -1,74 +1,81 @@
-// base path, that will be used to resolve files and exclude
-basePath = '../';
+module.exports = function(config) {
+  config.set({
+    // your config
+    // base path, that will be used to resolve files and exclude
+    basePath: '../',
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/e2e/**/*.js',
+      'app/js/**/*.js',
+    ],
 
 
-// list of files / patterns to load in the browser
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/**/*.js',
-  'app/js/**/*.js',
-];
+    // list of files to exclude
+    exclude: [
+    ],
 
 
-// list of files to exclude
-exclude = [
-];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    reporters: ['progress'],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // web server port
+    port: 7070,
 
 
-// web server port
-port = 7070;
+    // cli runner port
+    runnerPort: 7100,
 
 
-// cli runner port
-runnerPort = 7100;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO
+    // || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable watching file and executing tests whenever any file
+    // changes
+    autoWatch: true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
 
+    // use a subpath here, so we can test the full playground uri space
+    urlRoot: '/karma/',
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    proxies: {
+      '/': 'http://localhost:8080/'
+    },
 
-// use a subpath here, so we can test the full playground uri space
-urlRoot = '/karma/';
+    junitReporter: {
+      outputFile: 'test_out/e2e.xml',
+      suite: 'e2e'
+    },
 
-proxies = {
-  '/': 'http://localhost:8080/'
-};
+    frameworks: ["ng-scenario"]
 
-junitReporter = {
-  outputFile: 'test_out/e2e.xml',
-  suite: 'e2e'
+  });
 };
--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,71 +1,78 @@
-// base path, that will be used to resolve files and exclude
-basePath = '../';
+module.exports = function(config) {
+  config.set({
+    // your config
+    // base path, that will be used to resolve files and exclude
+    basePath: '../',
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-mocks.js',
-  'app/lib/angular-ui/angular-ui.js',
-  'app/lib/ui-bootstrap/*.js',
-  'app/js/**/*.js',
-  'test/unit/**/*.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'app/lib/angular/angular.js',
+      'app/lib/angular/angular-mocks.js',
+      'app/lib/angular-ui/angular-ui.js',
+      'app/lib/ui-bootstrap/*.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
 
 
-// list of files to exclude
-exclude = [
-];
+    // list of files to exclude
+    exclude: [
+    ],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    reporters: ['progress'],
 
 
-// web server port
-port = 6060;
+    // web server port
+    port: 6060,
 
 
-// cli runner port
-runnerPort = 6100;
+    // cli runner port
+    runnerPort: 6100,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO
+    // || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // enable / disable watching file and executing tests whenever any file
+    // changes
+    autoWatch: true,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
 
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    },
+
+    frameworks: ["jasmine"]
+  });
 };
